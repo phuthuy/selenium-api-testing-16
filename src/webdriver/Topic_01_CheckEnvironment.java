398902743ddb63e5f65a17f83dceda78d61ccf15
Nguyen Thi Thuy <nguyenthuy35nb@gmail.com>
2020-04-26 15:56:45 +0700

    Topic_01_CheckEnvironment

---
+++ src/webdriver/Topic_01_CheckEnvironment.java
@@ -0,0 +1,39 @@
+package webdriver;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_CheckEnvironment {
+	WebDriver driver;
+	
+  @Test
+  public void TC_01_CheckUrlAndTitle() {
+	  System.out.println("Check homepage title");
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle,"Guru99 Bank Home Page");
+	  
+	  System.out.println("Check homepage url");
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  driver.get("http://demo.guru99.com/v4/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
